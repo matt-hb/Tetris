1a0655d9c3879a34625846686c6dfe3738bc33a8
Hadházi-Borsos Mátyás <hbmatyi@gmail.com>
2024-11-21 17:54:03 +0100

    documented LeaderBoard class

--- src/scores/LeaderBoard.java
+++ src/scores/LeaderBoard.java
@@ -6,23 +6,48 @@ import java.util.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+/**
+ * The LeaderBoard class represents a leaderboard with a list of high scores.
+ */
 public class LeaderBoard {
+    /**
+     * A list of high scores in descending order.
+     */
     private List<HighScore> lb;
 
+    /**
+     * Constructs a new LeaderBoard object with an empty list of high scores.
+     */
     public LeaderBoard() {
         lb = new ArrayList<>();
     }
 
+    /**
+     * Constructs a new LeaderBoard object with the specified high scores, sorting them in descending order.
+     * @param scores the high scores to add to the leaderboard
+     */
     public LeaderBoard(HighScore... scores) {
         lb = new ArrayList<>(List.of(scores));
         Collections.sort(lb);
+        Collections.reverse(lb);
     }
 
+    /**
+     * Returns the high score at the specified index in the leaderboard.
+     * If the given index is higher than the number of high scores, returns an empty high score.
+     * @param index the index of the high score to return
+     * @return the high score at the specified index
+     */
     public HighScore get(int index) {
         if (index >= lb.size()) return new HighScore();
         return lb.get(index);
     }
 
+    /**
+     * Adds a new high score to the leaderboard, sorting the list in descending order.
+     * If the score is invalid, it is not added.
+     * @param s the high score to add
+     */
     public void add(HighScore s) {
         if (s.getScore() != 0) {
             lb.add(s);
@@ -31,10 +56,11 @@ public class LeaderBoard {
         }
     }
 
-    public int size() {
-        return lb.size();
-    }
-
+    /**
+     * Returns the position the given score would have in the leaderboard, if added to it.
+     * @param score the score to check
+     * @return the position the score would have in the leaderboard
+     */
     public int positionIfAdded(int score) {
         int pos = 0;
         while (pos < lb.size() && lb.get(pos).getScore() >= score) {
@@ -43,6 +69,11 @@ public class LeaderBoard {
         return (pos+1);
     }
 
+    /**
+     * Reads high scores from a JSON file and adds them to the leaderboard.
+     * The file should only contain valid high scores, in descending order.
+     * @param filename the name of the file to read from
+     */
     public void readFromJSON(String filename) {
         Gson gson = new Gson();
         try  {
@@ -55,6 +86,11 @@ public class LeaderBoard {
         }
     }
 
+    /**
+     * Writes the high scores to a JSON file.
+     * The file will contain the high scores in descending order.
+     * @param filename the name of the file to write to
+     */
     public void writeToJSON(String filename) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         try {
